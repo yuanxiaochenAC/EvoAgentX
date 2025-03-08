c99b639656182e5e600217d7268380505957b5d8
jinyuan <fangjy6@gmail.com>
2025-03-08 19:31:20 +0000

    add GSM8K benchmark

--- evoagentx/benchmark/__init__.py
+++ evoagentx/benchmark/__init__.py
@@ -1,5 +1,6 @@
 from .nq import NQ 
 from .hotpotqa import HotPotQA
+from .gsm8k import GSM8K
 from .mbpp import MBPP
 
-__all__ = ["NQ", "HotPotQA", "MBPP"]
+__all__ = ["NQ", "HotPotQA", "MBPP", "GSM8K"]
