6a008573808211db8d4462d4df14c8eaabc48b57
jinyuan <fangjy6@gmail.com>
2025-04-19 23:20:31 +0100

    fix Agent unittests and update input/output formats in existing actions

--- evoagentx/actions/agent_generation.py
+++ evoagentx/actions/agent_generation.py
@@ -106,8 +106,10 @@ class AgentGeneration(Action):
         name = kwargs.pop("name") if "name" in kwargs else AGENT_GENERATION_ACTION["name"]
         description = kwargs.pop("description") if "description" in kwargs else AGENT_GENERATION_ACTION["description"]
         prompt = kwargs.pop("prompt") if "prompt" in kwargs else AGENT_GENERATION_ACTION["prompt"]
-        inputs_format = kwargs.pop("inputs_format") if "inputs_format" in kwargs else AgentGenerationInput
-        outputs_format = kwargs.pop("outputs_format") if "outputs_format" in kwargs else AgentGenerationOutput
+        # inputs_format = kwargs.pop("inputs_format") if "inputs_format" in kwargs else AgentGenerationInput
+        # outputs_format = kwargs.pop("outputs_format") if "outputs_format" in kwargs else AgentGenerationOutput
+        inputs_format = kwargs.pop("inputs_format", None) or AgentGenerationInput
+        outputs_format = kwargs.pop("outputs_format", None) or AgentGenerationOutput 
         super().__init__(name=name, description=description, prompt=prompt, inputs_format=inputs_format, outputs_format=outputs_format, **kwargs)
     
     def execute(self, llm: Optional[BaseLLM] = None, inputs: Optional[dict] = None, sys_msg: Optional[str]=None, return_prompt: bool = False, **kwargs) -> AgentGenerationOutput:
