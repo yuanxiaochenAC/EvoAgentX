24e2c2baf6db593b7dca9ba8424f5745ef894bef
bruuuuuceliu <bruceliu1788@outlook.com>
2025-05-30 23:21:41 +0800

    update from main

--- evoagentx/core/module.py
+++ evoagentx/core/module.py
@@ -59,7 +59,7 @@ class BaseModule(BaseModel, metaclass=MetaModule):
     """
 
     class_name: str = None 
-    model_config = {"arbitrary_types_allowed": True, "extra": "allow", "protected_namespaces": (), "validate_assignment": True}
+    model_config = {"arbitrary_types_allowed": True, "extra": "allow", "protected_namespaces": (), "validate_assignment": False}
 
     def __init_subclass__(cls, **kwargs):
         """
