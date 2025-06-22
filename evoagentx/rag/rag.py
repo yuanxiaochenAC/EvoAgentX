e77a72e3c741dcb486751fd785737b085b2e5056
Zijian <61169715+Dylonsword@users.noreply.github.com>
2025-06-22 11:40:14 +0800

    fix format in rag.py

--- evoagentx/rag/rag.py
+++ evoagentx/rag/rag.py
@@ -54,7 +54,7 @@ class RAGEngine:
         # Dynamic Check the dimensions in StorageHandler
         if (self.storage_handler.vector_store is not None) and (self.embed_model.dimensions is not None):
             if self.storage_handler.vector_store.dimensions != self.embed_model.dimensions:
-                logger.warning(f"The dimensions in vector_store is not equal with embed_model. Reiniliaze vector_store.")
+                logger.warning("The dimensions in vector_store is not equal with embed_model. Reiniliaze vector_store.")
                 self.storage_handler.storageConfig.vectorConfig.dimensions = self.embed_model.dimensions
                 self.storage_handler._init_vector_store()
 
