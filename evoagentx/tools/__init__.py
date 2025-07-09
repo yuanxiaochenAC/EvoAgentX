7658eb29db20868d83af986fe5fde9da33fb10a5
bruuuuuceliu <bruceliu1788@outlook.com>
2025-07-09 19:38:10 +0600

    add new tools

--- evoagentx/tools/__init__.py
+++ evoagentx/tools/__init__.py
@@ -10,6 +10,8 @@ from .mcp import MCPToolkit
 from .request import RequestToolkit
 from .request_arxiv import ArxivToolkit
 from .browser_use import BrowserUseToolkit
+from .database_toolkit import DatabaseToolkit
+from .database_mongodb import MongoDBToolkit
 
 
 __all__ = [
@@ -25,6 +27,8 @@ __all__ = [
     "MCPToolkit",
     "RequestToolkit",
     "ArxivToolkit",
-    "BrowserUseToolkit"
+    "BrowserUseToolkit",
+    "DatabaseToolkit",
+    "MongoDBToolkit"
 ]
 
