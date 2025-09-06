e06b40fae6ed6b9b34045830a952a369f6bb75bf
Billy Bao <newyorkupperbay@gmail.com>
2025-09-06 16:38:46 +0800

    Update utils.py

--- evoagentx/frameworks/multi_agent_debate/utils.py
+++ evoagentx/frameworks/multi_agent_debate/utils.py
@@ -1,4 +1,3 @@
-from typing import List, Dict, Any
 from ...prompts.workflow.multi_agent_debate import format_transcript, collect_last_round_candidates, collect_round_candidates
 
 # Re-export functions for backward compatibility
